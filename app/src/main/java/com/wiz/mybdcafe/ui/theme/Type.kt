a35cbe85c1578798a60a0dec476dcac5ba79a508
Lee Jihye <jihae0584@gmail.com>
2024-02-06 19:24:10 +0900

    :lipstick: NanumNeo 폰트 패밀리 추가

--- app/src/main/java/com/wiz/mybdcafe/ui/theme/Type.kt
+++ app/src/main/java/com/wiz/mybdcafe/ui/theme/Type.kt
@@ -2,14 +2,25 @@ package com.wiz.mybdcafe.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.wiz.mybdcafe.R
+
+val NanumNeo = FontFamily(
+    Font(R.font.nanum_square_neo_alt, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.nanum_square_neo_brg, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.nanum_square_neo_cbd, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.nanum_square_neo_deb, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.nanum_square_neo_ehv, FontWeight.Black, FontStyle.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NanumNeo,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
