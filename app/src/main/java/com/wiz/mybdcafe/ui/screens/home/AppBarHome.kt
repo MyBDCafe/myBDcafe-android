7a06b0e54601cb6e6072a7b64e6ecb4fda28bf10
Lee Jihye <jihae0584@gmail.com>
2024-02-06 19:29:49 +0900

    :lipstick: 메인 화면 앱바 그림

---
+++ app/src/main/java/com/wiz/mybdcafe/ui/screens/home/AppBarHome.kt
@@ -0,0 +1,48 @@
+package com.wiz.mybdcafe.ui.screens.home
+
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wiz.mybdcafe.R
+import com.wiz.mybdcafe.ui.theme.NanumNeo
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBarHome() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name_kr),
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .wrapContentHeight(align = Alignment.CenterVertically),
+                color = colorResource(id = R.color.red_0),
+                fontFamily = NanumNeo,
+                fontWeight = FontWeight.Black,
+                textAlign = TextAlign.Center
+            )
+        },
+        modifier = Modifier
+            .height(52.dp)
+            .fillMaxWidth(),
+    )
+}
+
+@Preview
+@Composable
+private fun AppbarHomePreview() {
+    AppBarHome()
+}
