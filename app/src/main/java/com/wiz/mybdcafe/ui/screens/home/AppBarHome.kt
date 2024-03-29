b30468f9b18ec6d554e63b4dd0a576908cef9b66
Lee Jihye <jihae0584@gmail.com>
2024-03-30 03:42:48 +0900

    :bug: 앱바 배경색이 필터 배경과 미세하게 다른 오류 수정 #1

--- app/src/main/java/com/wiz/mybdcafe/ui/screens/home/AppBarHome.kt
+++ app/src/main/java/com/wiz/mybdcafe/ui/screens/home/AppBarHome.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,8 +42,10 @@ fun AppBarHome() {
         },
         modifier = Modifier
             .height(52.dp)
-            .fillMaxWidth()
-            .background(Color.White),
+            .fillMaxWidth(),
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color.White
+        )
     )
 }
 
