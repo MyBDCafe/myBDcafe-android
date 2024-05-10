e6e05fba290f47a4150cd9b86c20ce892a78dfa5
Lee Jihye <jihae0584@gmail.com>
2024-05-10 20:05:23 +0900

    :sparkles: 이벤트 등록/수정 API 데이터 클래스 작성 #9

---
+++ app/src/main/java/com/wiz/mybdcafe/viewModel/Event.kt
@@ -0,0 +1,74 @@
+package com.wiz.mybdcafe.viewModel
+
+import com.google.gson.annotations.SerializedName
+import java.time.LocalDateTime
+import java.util.Date
+
+/*
+* 이벤트 등록 요청/응답
+*/
+
+data class EventAddRequest(
+    val eventName: String,
+    val mainCharacter: String? = null,
+    val genre: String? = null,
+    val startDate: Date? = null,
+    val endDate: Date? = null,
+    val location: Location.Basic,
+    val businessHours: List<BusinessHour.Basic> = listOf(),
+    val eventUrl: String? = null,
+    val memo: String? = null
+)
+
+data class EventAddResponse(
+    val status: Int,
+    val timestamp: LocalDateTime,
+    val errorClass: String,
+
+    @SerializedName(value = "errMsg")
+    val errorMessage: String
+)
+
+/*
+* 이벤트 수정 요청/응답
+*/
+
+data class EventUpdateRequest(
+    val eventName: String,
+    val mainCharacter: String? = null,
+    val genre: String? = null,
+    val startDate: Date? = null,
+    val endDate: Date? = null,
+    val location: Location.ID,
+    val businessHours: List<BusinessHour.ID> = listOf(),
+    val eventUrl: String? = null,
+    val memo: String? = null
+)
+
+sealed class Location {
+    data class Basic(
+        val latitude: String? = null,
+        val longitude: String? = null
+    )
+
+    data class ID(
+        val locationId: Long,
+        val latitude: String? = null,
+        val longitude: String? = null
+    )
+}
+
+sealed class BusinessHour {
+    data class Basic(
+        val day: Date? = null,
+        val openTime: Date? = null,
+        val closeTime: Date? = null
+    )
+
+    data class ID(
+        val hourId: Long,
+        val day: Date? = null,
+        val openTime: Date? = null,
+        val closeTime: Date? = null
+    )
+}
